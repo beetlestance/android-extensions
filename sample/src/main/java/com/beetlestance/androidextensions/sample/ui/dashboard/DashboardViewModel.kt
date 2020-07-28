a8c1ad9e3b4ba1fdf3e8cef1f95bf41a3177da95
akshay <akshay253101yadav@gmail.com>
2020-07-28 09:58:40 +0530

    sample code cleanup

--- sample/src/main/java/com/beetlestance/androidextensions/sample/ui/dashboard/DashboardViewModel.kt
+++ sample/src/main/java/com/beetlestance/androidextensions/sample/ui/dashboard/DashboardViewModel.kt
@@ -3,10 +3,8 @@ package com.beetlestance.androidextensions.sample.ui.dashboard
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import com.beetlestance.androidextensions.navigation.NavigateOnceDirectionRequest
-import com.beetlestance.androidextensions.sample.TopLevelNavigatorViewModelDelegate
 
-class DashboardViewModel @ViewModelInject constructor() :
-    ViewModel() {
+class DashboardViewModel @ViewModelInject constructor() : ViewModel() {
 
     fun navigateToNotificationFragment(): NavigateOnceDirectionRequest {
         return NavigateOnceDirectionRequest(
--- sample/src/main/java/com/beetlestance/androidextensions/sample/ui/notifications/NotificationsViewModel.kt
+++ sample/src/main/java/com/beetlestance/androidextensions/sample/ui/notifications/NotificationsViewModel.kt
@@ -1,14 +1,6 @@
 package com.beetlestance.androidextensions.sample.ui.notifications
 
-import androidx.core.net.toUri
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
-import com.beetlestance.androidextensions.navigation.NavigateOnceDeeplinkRequest
-import com.beetlestance.androidextensions.sample.TopLevelNavigatorViewModelDelegate
-import com.beetlestance.androidextensions.sample.constants.FEED_DEEPLINK
-import com.beetlestance.androidextensions.sample.constants.HOME_DEEPLINK
-import com.beetlestance.androidextensions.sample.constants.SEARCH_DEEPLINK
-import com.beetlestance.androidextensions.sample.event.Event
 
-class NotificationsViewModel @ViewModelInject constructor() :
-    ViewModel()
+class NotificationsViewModel @ViewModelInject constructor() : ViewModel()
