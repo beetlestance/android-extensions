85da723b84e1bacfbf81cace7105e4b5f77292b3
akshay yadav <akshay253101yadav@gmail.com>
2021-01-26 11:01:40 +0530

    fragment resource are marked private since 1.3.0 beta01, switching back to nav_defaults

--- navigation/src/main/java/com/beetlestance/androidextensions/navigation/data/NavAnimations.kt
+++ navigation/src/main/java/com/beetlestance/androidextensions/navigation/data/NavAnimations.kt
@@ -3,8 +3,8 @@ package com.beetlestance.androidextensions.navigation.data
 import com.beetlestance.androidextensions.navigation.R
 
 data class NavAnimations(
-    val enterAnimation: Int = R.anim.fragment_open_enter,
-    val exitAnimation: Int = R.anim.fragment_open_exit,
-    val popEnterAnimation: Int = R.anim.fragment_close_enter,
-    val popExitAnimation: Int = R.anim.fragment_close_exit
+    val enterAnimation: Int = R.anim.nav_default_enter_anim,
+    val exitAnimation: Int = R.anim.nav_default_exit_anim,
+    val popEnterAnimation: Int = R.anim.nav_default_pop_enter_anim,
+    val popExitAnimation: Int = R.anim.nav_default_pop_exit_anim
 )
